cb3d3fc73c03ad35ee2c4d35a74208ecb65782ba
breadbomb121 <petertberndtsson@gmail.com>
2025-10-04 13:59:43 -0600

    created base frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-800">
+        <RouterView></RouterView> 
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import InputForm from './components/InputForm.vue'
+import { RouterView } from "vue-router"
+
+
+</script>
+
---
+++ frontend/src/routes/HomeView.vue
@@ -0,0 +1,14 @@
+<template>  
+    <div class="min-h-screen flex flex-col bg-gray-800">
+        <Header /> 
+        <main class="flex-1 p-6 bg-gray-800 text-white">
+            <InputForm></InputForm>
+        </main>
+    </div>
+</template>
+
+
+<script setup>
+    import Header from "../components/Header.vue"
+    import InputForm from "../components/InputForm.vue"
+</script>
